117eb0a33f5c1f7a9a6f89f3e1062f9cec008aa5
Vacxe <aksenov.kostya@gmail.com>
2017-05-10 15:38:51 +0300

    Beta Service Integration [skip ci]

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,6 @@
 cd ..
 GRADLE="./gradlew"
 $GRADLE assembleRelease
+$GRADLE crashlyticsUploadDistributionRelease
 RELEASEFILE="${CIRCLE_ARTIFACTS}/habrcicdcat-release-${CIRCLE_BUILD_NUM}-${CIRCLE_SHA1}.apk"
 cp ./app/build/outputs/apk/app-release.apk ${RELEASEFILE}
